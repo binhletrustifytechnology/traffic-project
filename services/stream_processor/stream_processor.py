97ef8c07af4dcbeb514e02cb7d992d1cd8efea39
binh.le <binh.le@trustifytechnology.com>
2025-12-31 18:53:19 +0700

    init

--- services/stream_processor/stream_processor.py
+++ services/stream_processor/stream_processor.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import from_json, col, window, avg, count, expr
+from pyspark.sql.functions import from_json, col, window, avg, count, expr, to_json, struct, lit
 from pyspark.sql.types import StructType, StructField, StringType, DoubleType, TimestampType
 
 # 1. Start Spark session
@@ -26,42 +26,54 @@ raw_df = (spark.readStream
           .option("subscribe", "traffic_stream")
           .load())
 
+#
+# # 4. Parse JSON payload
+# json_df = raw_df.select(from_json(col("value").cast("string"), schema).alias("data")).select("data.*")
+#
+# # 5. Cast timestamp field
+# events = json_df.withColumn("ts", col("timestamp").cast(TimestampType()))
+#
+# # 6. Windowed aggregations (2‑minute tumbling windows)
+# agg = (events.groupBy(
+#             window(col("ts"), "2 minutes"),
+#             col("road_segment_id")
+#         )
+#         .agg(
+#             avg("speed").alias("avg_speed"),
+#             count("*").alias("vehicle_count"),
+#             expr("sum(case when speed < 20 then 1 else 0 end)/count(*)").alias("percent_slow")
+#         ))
+#
+# # 7. Flatten window columns
+# out = agg.selectExpr(
+#     "road_segment_id",
+#     "CAST(window.start AS STRING) AS window_start",
+#     "CAST(window.end AS STRING) AS window_end",
+#     "CAST(avg_speed AS DOUBLE)",
+#     "CAST(vehicle_count AS INT)",
+#     "CAST(percent_slow AS DOUBLE)"
+# )
+#
+# # Convert all columns into a JSON string
+# out_kafka = out.select(
+#     col("road_segment_id").alias("key"), # optional, use segment ID as key
+#     to_json(struct(
+#         col("road_segment_id"),
+#         col("window_start"),
+#         col("window_end"),
+#         col("avg_speed"),
+#         col("vehicle_count"),
+#         col("percent_slow")
+#     )).alias("value") )
+#
+#
+# # 8. Write results back to Kafka (segment_metrics topic)
+# query = (out.writeStream
+#          .format("kafka")
+#          .option("kafka.bootstrap.servers", "kafka:9092")
+#          .option("topic", "segment_metrics")
+#          .option("checkpointLocation", "/tmp/spark_chkpt")
+#          .outputMode("update")
+#          .start())
 
-# 4. Parse JSON payload
-json_df = raw_df.select(from_json(col("value").cast("string"), schema).alias("data")).select("data.*")
-
-# 5. Cast timestamp field
-events = json_df.withColumn("ts", col("timestamp").cast(TimestampType()))
-
-# 6. Windowed aggregations (2‑minute tumbling windows)
-agg = (events.groupBy(
-            window(col("ts"), "2 minutes"),
-            col("road_segment_id")
-        )
-        .agg(
-            avg("speed").alias("avg_speed"),
-            count("*").alias("vehicle_count"),
-            expr("sum(case when speed < 20 then 1 else 0 end)/count(*)").alias("percent_slow")
-        ))
-
-# 7. Flatten window columns
-out = agg.selectExpr(
-    "road_segment_id",
-    "CAST(window.start AS STRING) AS window_start",
-    "CAST(window.end AS STRING) AS window_end",
-    "CAST(avg_speed AS DOUBLE)",
-    "CAST(vehicle_count AS INT)",
-    "CAST(percent_slow AS DOUBLE)"
-)
-
-
-# 8. Write results back to Kafka (segment_metrics topic)
-query = (out.writeStream
-         .format("kafka")
-         .option("kafka.bootstrap.servers", "kafka:9092")
-         .option("topic", "segment_metrics")
-         .option("checkpointLocation", "/tmp/spark_chkpt")
-         .outputMode("update")
-         .start())
-
-query.awaitTermination()
+# query.awaitTermination()
