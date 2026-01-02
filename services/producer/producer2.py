466583daca586fc80722d25d84951df176aaf17e
binh.le <binh.le@trustifytechnology.com>
2026-01-02 17:05:18 +0700

    init

--- services/producer/producer2.py
+++ services/producer/producer2.py
@@ -1,7 +1,8 @@
 import json
-import time
 import random
-from datetime import datetime
+import time
+import uuid
+
 from kafka import KafkaProducer
 
 # Configure Kafka producer
@@ -13,14 +14,14 @@ producer = KafkaProducer(
 def simulate_event():
     """Generate a synthetic traffic event."""
     return {
-        "vehicle_id": f"veh_{random.randint(1000,9999)}",
-        "timestamp": datetime.utcnow().isoformat(),
-        "lat": 10.76 + random.uniform(-0.01, 0.01),   # around HCMC
-        "lon": 106.68 + random.uniform(-0.01, 0.01),
-        "speed": max(0, random.gauss(35, 10)),        # km/h
-        "heading": random.randint(0, 359),
-        "road_segment_id": random.choice(["A1","A2","B7","C3"]),
-        "weather_code": random.choice([0,1,2])        # 0=clear, 1=rain, 2=heavy rain
+        "id": str(uuid.uuid4()),
+        "serial_number": ['0x0004B182', '0x0004B0D0', '0x0004B1BA'][random.randint(0, 2)],
+        "message_number": [random.randint(0, 2)],
+        "timestamp_seconds": int(time.time()),
+        "lane": random.randint(0, 2),
+        "vehicle_class": random.randint(0, 2),
+        "vehicle_volume": random.randint(0, 10),
+        "vehicle_avg_speed": random.choice([3,5,7])
     }
 
 def main():
@@ -29,7 +30,7 @@ def main():
         event = simulate_event()
         producer.send("traffic_stream", event)
         print(f"Produced event: {event}")
-        time.sleep(1)  # adjust frequency as needed
+        time.sleep(0.5)  # adjust frequency as needed
 
 if __name__ == "__main__":
     main()
--- services/stream_processor/stream_processor.py
+++ services/stream_processor/stream_processor.py
@@ -1,6 +1,7 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import from_json, to_json, struct, col
+from pyspark.sql.functions import col, from_json
 from pyspark.sql.types import StructType, StructField, IntegerType, DoubleType
+
 
 # 1. Initialize Spark with the Kafka Connector
 spark = SparkSession.builder \
@@ -16,15 +17,34 @@ schema = StructType([
 
 # 3. Read from Kafka
 # Note: 'kafka:9092' is used because Spark is running inside the Docker network
-raw_df = spark.read \
+raw_df = spark.readStream \
     .format("kafka") \
     .option("kafka.bootstrap.servers", "kafka:9092") \
     .option("startingOffsets", "earliest") \
     .option("subscribe", "my-topic") \
     .load()
 
-df1 = raw_df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
+df1 = raw_df.selectExpr("CAST(value AS STRING)")
+# df1.show()
 
-df1.show()
+# json_df = df1.select(from_json(col("value"), schema).alias("data")).select("data.*")
+
+# Perform transformations
+# transformed_df = json_df.withColumn("event_value_doubled", col("event_value") * 2)
+
+# transformed_df.show()
+
+query = (raw_df
+         .writeStream
+         .format("kafka")
+         .option("kafka.bootstrap.servers", "kafka:9092")
+         .option("topic", "out-topic")
+         .option("checkpointLocation", "/tmp/spark_chkpt")
+         .outputMode("update")
+         .start())
+
+# Write the transformed data to console (for simplicity)
+# query = transformed_df.writeStream.outputMode("append").format("console").start()
+query.awaitTermination()
 
 # /opt/spark/bin/spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.13:4.0.0 stream_processor.py
